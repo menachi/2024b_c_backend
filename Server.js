71e75eae55faad48e7ba38f1687b6454b63d839b
Eliav Menachi <eliav.menachi@gmail.com>
2024-03-20 21:55:54 +0200

    Update Server.js

--- Server.js
+++ Server.js
@@ -4,6 +4,7 @@ init().then((app) => {
   app.listen(process.env.PORT, () => {
     console.log(
       "Example app listening at http://localhost:" + process.env.PORT
+      console.log("asdfasdfas");
     );
   });
 });
