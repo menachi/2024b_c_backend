837e81b394c4b4769d33f34463489ab1b26cc54b
Eliav Menachi <eliav.menachi@gmail.com>
2024-03-20 21:53:13 +0200

    jest

--- App.js
+++ App.js
@@ -2,22 +2,26 @@ const express = require("express");
 const app = express();
 const studentRoute = require("./routes/student_route");
 const postRoute = require("./routes/post_route");
-const { mongo } = require("mongoose");
+const env = require("dotenv").config();
 
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-mongoose.connect(process.env.DATABASE_URL);
-const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("connected to database"));
+const init = () => {
+  const promise = new Promise((resolve, reject) => {
+    const db = mongoose.connection;
+    db.on("error", (error) => console.error(error));
+    db.once("open", () => console.log("connected to database"));
+    mongoose.connect(process.env.DATABASE_URL).then(() => {
+      app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+      app.use("/student", studentRoute);
+      app.use("/post", postRoute);
+      resolve(app);
+    });
+  });
+  return promise;
+};
 
-app.use("/student", studentRoute);
-app.use("/post", postRoute);
-
-app.listen(process.env.PORT, () => {
-  console.log("Example app listening at http://localhost:" + process.env.PORT);
-});
+module.exports = init;
---
+++ Server.js
@@ -0,0 +1,9 @@
+const init = require("./App.js");
+
+init().then((app) => {
+  app.listen(process.env.PORT, () => {
+    console.log(
+      "Example app listening at http://localhost:" + process.env.PORT
+    );
+  });
+});
