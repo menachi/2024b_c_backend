2dabadf09df8b5a3b2c82863b26a39dfc3a80777
Eliav Menachi <eliav.menachi@gmail.com>
2024-03-27 08:00:49 +0200

    ...

--- Server.js
+++ Server.js
@@ -4,7 +4,6 @@ init().then((app) => {
   app.listen(process.env.PORT, () => {
     console.log(
       "Example app listening at http://localhost:" + process.env.PORT
-      console.log("asdfasdfas");
     );
     // this is my comment
     console.log("Hello World");
